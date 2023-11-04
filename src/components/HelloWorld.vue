e479f9c0a8f0d406de3d10beeb6a7e792cdf8898
felipesagredo <felipesagredogalindo@gmail.com>
2023-11-04 17:34:12 -0300

    Diseño

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,8 @@
 <template>
-  <div>
-    <div id="map" class="map-container" style="height: 400px;"></div>
-    <h1>Buscador de Sismos alrededor del Mundo</h1>
-    <div>
+  <div class="origin">
+    <div id="map" class="map-container" style="height: 500px;"></div>
+    <h1>Buscador de movimientos telúricos</h1>
+    <div class="input-container">
       <label for="fechaInicio">Fecha de Inicio:</label>
       <input type="date" id="fechaInicio" v-model="fechaInicio" />
       <label for="fechaTermino">Fecha de Término:</label>
@@ -11,11 +11,22 @@
       <input type="text" id="magnitud" v-model="magnitud" />
       <button @click="buscarTerremotos">Buscar</button>
     </div>
-    <ul v-if="earthquakes.length">
-      <li v-for="earthquake in earthquakes" :key="earthquake.id">
-        {{ earthquake.properties.place }} - Magnitud: {{ earthquake.properties.mag }} - Fecha: {{ formatDate(earthquake.properties.time) }}
-      </li>
-    </ul>
+    <table class="justified-table" v-if="earthquakes.length">
+      <thead>
+        <tr>
+          <th>Lugar</th>
+          <th>Magnitud</th>
+          <th>Fecha</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="earthquake in earthquakes" :key="earthquake.id">
+          <td>{{ earthquake.properties.place }}</td>
+          <td>{{ earthquake.properties.mag }}</td>
+          <td>{{ formatDate(earthquake.properties.time) }}</td>
+        </tr>
+      </tbody>
+</table>
     <p v-else>No se encontraron Sismos para el rango de fechas seleccionado.</p>
   </div>
 </template>
@@ -41,7 +52,7 @@ export default {
     this.map = L.map('map').setView([-36.820087, -73.044280], 13);
     // Agrega una capa de mapa base, como OpenStreetMap
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map);
-    this.popupTemplate = L.popup();
+    this.popupTemplate = L.popup(this.popupContent);
 },
   methods: {
     buscarTerremotos() {
@@ -61,23 +72,20 @@ export default {
             });
             // Agrega marcadores para los terremotos
             this.earthquakes.forEach(earthquake => {
-            const coordinates = earthquake.geometry.coordinates;
-            const latlng = [coordinates[1], coordinates[0]]; // Invertir las coordenadas
-            const popupContent = `
-              <strong>${earthquake.properties.place}</strong><br>
-              Magnitud: ${earthquake.properties.mag}<br>
-              Fecha: ${this.formatDate(earthquake.properties.time)}
-            `;
-              // Agrega un círculo con propiedades personalizadas
-              // Utiliza la plantilla del popup previamente creada
-              const popup = this.popupTemplate
-                .setContent(popupContent);
-              // Agrega un círculo con el popup
-              L.circle(latlng, {
-                radius: earthquake.properties.mag * 5000,
-              })
-                .bindPopup(popup) // Vincula el popup aquí
-                .addTo(this.map);
+              const coordinates = earthquake.geometry.coordinates;
+              const latlng = [coordinates[1], coordinates[0]]; // Invertir las coordenadas
+              const popupContent = `
+                <strong>${earthquake.properties.place}</strong><br>
+                Magnitud: ${earthquake.properties.mag}<br>
+                Fecha: ${this.formatDate(earthquake.properties.time)}
+              `;
+                // Agrega un círculo con propiedades personalizadas
+                // Utiliza la plantilla del popup previamente creada
+                const popup = this.popupTemplate.setContent(popupContent);
+                // Agrega un círculo con el popup
+                L.circleMarker(latlng, {
+                  radius: earthquake.properties.mag * 5,
+                }).bindPopup(popup).addTo(this.map);
             });
           })
           .catch(error => {
@@ -96,8 +104,45 @@ export default {
 <style scoped>
 
 .map-container {
+  margin-top: 0;
   width: 100%; /* Ajusta el ancho según tus necesidades */
   height: 400px; /* Ajusta la altura según tus necesidades */
 }
 
+.justified-table {
+  margin: auto;
+  margin-top: 20px;
+  width: fit-content;
+  min-width: 100%;
+  border-collapse: collapse;
+}
+
+.justified-table th, .justified-table td {
+  text-align: justify;
+  padding: 8px;
+  border: 1px solid #dddddd;
+}
+
+.justified-table th {
+  background-color: #f2f2f2;
+}
+
+.justified-table tr:nth-child(even) {
+  background-color: #f2f2f2;
+}
+
+.justified-table tr:nth-child(odd) {
+  background-color: #ffffff;
+}
+.input-container{
+  text-align: justify;
+  margin: auto;
+  display: grid;
+  width: 200px;
+  height: fit-content;
+}
+
+input {
+  margin-bottom: 10px;
+}
 </style>
